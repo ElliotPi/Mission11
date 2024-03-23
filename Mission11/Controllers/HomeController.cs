308c6e970b65d0756e14221a91cd484159df4ae1
ElliotPi <103081209+ElliotPi@users.noreply.github.com>
2024-03-22 23:16:28 -0600

    Comment my codes

--- Mission11/Controllers/HomeController.cs
+++ Mission11/Controllers/HomeController.cs
@@ -18,21 +18,21 @@ namespace Mission11.Controllers
         {
             int pageSize = 10;
 
-            var BookListInfo = new BookListViewModel
+            var BookListInfo = new BookListViewModel 
             {
                 Books = _repo.Books
                     .OrderBy(x => x.BookId)
-                    .Skip((pageNum - 1) * pageSize)
-                    .Take(pageSize),
-                PaginationInfo = new PaginationInfo
-                {
+                    .Skip((pageNum - 1) * pageSize)     // if pageNum is 1 (first page) and pageSize is 10, the calculation would be (1 - 1) * 10 = 0,
+                    .Take(pageSize),                    // meaning no items are skipped for the first page. For the second page (pageNum = 2), 
+                PaginationInfo = new PaginationInfo     // the calculation would be (2 - 1) * 10 = 10,
+                {                                       // so the query would skip the first 10 items to get to the items for the second page.
                     CurrentPage = pageNum,
                     ItemsPerPage = pageSize,
                     TotalItems = _repo.Books.Count()
                 }
             };
    
-            return View(BookListInfo);
+            return View(BookListInfo); // Passing all these info to the view
         }
 
     }
--- Mission11/Models/ViewModels/BookListViewModel.cs
+++ Mission11/Models/ViewModels/BookListViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mission11.Models.ViewModels
 {
-    public class BookListViewModel
+    public class BookListViewModel  // This ViewModel contains all the model we need for the Index view
     {
         public IQueryable<Book> Books { get; set;}
         public PaginationInfo PaginationInfo { get; set;} = new PaginationInfo();
